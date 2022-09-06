16d4bd93470d700f1fbbc15bf79ac722eb6e0068
Oren Collaco <orencollaco97@gmail.com>
2022-09-06 15:20:47 -0400

    Modified to split tmux and simultaneously run vue and the SAS HTTPS server

--- start_opensas.sh
+++ start_opensas.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tmux new -A -s opensas 'cd Core; python3 HttpsServer.py'
+tmux new -A -s opensas-server 'cd OpenSAS-dashboard; npm run dev'\; split-window -h "cd Core; python3 HttpsServer.py"
