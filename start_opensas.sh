024e352a20c79df442ee888974683e9c78a4b78d
Oren Collaco <orencollaco97@gmail.com>
2022-11-13 21:48:58 -0500

    Changed start script

--- start_opensas.sh
+++ start_opensas.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tmux new -A -s opensas-server 'cd OpenSAS-dashboard; npm run dev'\; split-window -h "cd Core; python3 HttpsServer.py"
+tmux new -A -s opensas-server 'cd OpenSAS-dashboard; npm run dev'\; split-window -h "cd Core; python3 server.py"
 
